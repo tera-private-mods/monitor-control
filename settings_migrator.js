5dc522761ab86fcb436787e14112cfe84db31083
Dmitry S <dmitry.s@hsdn.org>
2024-08-28 15:31:52 +0400

    Update AS ids for RK-9

--- settings_migrator.js
+++ settings_migrator.js
@@ -7,8 +7,8 @@ const DefaultSettings = {
     "abnormalblock": [4850, 48732, 48734, 48735, 48736, 48737, 48738, 70251, 70252, 70253, 70254, 70255, 70256, 70465, 476806, 630201, 630202, 630411, 630511, 631002, 631003, 631201, 631202, 776017, 806001, 806002, 811061, 821101, 905656, 905657, 7102001, 45000011, 45000012, 47660800, 47660900, 47661000, 47662300, 999001011],
     "crystalblock": [10306, 10316, 12001, 12003, 12120, 12130],
     "skillblock": [101300, 101301],
-    "asnoblock": [40000, 40001, 40002, 426002, 3107001, 3107002, 3107003],
-}
+    "asnoblock": [40000, 40001, 40002, 426002, 3107001, 3107002, 3107003, 3107005, 3107006, 3107007]
+};
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
     if (from_ver === undefined) return { ...DefaultSettings, ...settings };
@@ -21,15 +21,16 @@ module.exports = function MigrateSettings(from_ver, to_ver, settings) {
             return MigrateSettings(from_ver + 1, to_ver, settings);
         }
         const oldsettings = settings;
+        settings = Object.assign(DefaultSettings, {});
         switch (to_ver) {
-            default:
-                settings = Object.assign(DefaultSettings, {});
-
-                for (const option in oldsettings) {
-                    if (settings[option] !== undefined) {
-                        settings[option] = oldsettings[option];
-                    }
-                }
+            case 3:
+                oldsettings["asnoblock"] = settings["asnoblock"];
+                break;
+        }
+        for (const option in oldsettings) {
+            if (settings[option] !== undefined) {
+                settings[option] = oldsettings[option];
+            }
         }
         return settings;
     }
